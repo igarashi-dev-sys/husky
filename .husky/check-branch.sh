94a2376cc451313a55801876f6e15004694ba4c3
igarashi-dev-sys <hiroshi.igarashi@uni.iret.co.jp>
2024-10-10 11:33:08 +0900

    push huuk test

--- .husky/check-branch.sh
+++ .husky/check-branch.sh
@@ -4,10 +4,10 @@
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 
 # 禁止するブランチ（例: develop ブランチ）
-if [[ "$current_branch" == "develop" ]]; then
-  echo "エラー: $current_branch ブランチへのコミットは禁止されています。"
-  exit 1
-else
-  echo "ブランチ $current_branch でのコミットを許可します。"
-  exit 0
-fi
+# if [[ "$current_branch" == "develop" ]]; then
+#   echo "エラー: $current_branch ブランチへのコミットは禁止されています。"
+#   exit 1
+# else
+#   echo "ブランチ $current_branch でのコミットを許可します。"
+#   exit 0
+# fi
